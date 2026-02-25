4206de3059d853f74c50160e28c653ed7461ac57
juanCamilo2002 <juan2002djfh@gmail.com>
2026-02-25 13:35:25 -0500

    Initial backend setup with Django, DRF, environment configuration and secure secret key

---
+++ apps/users/models.py
@@ -0,0 +1,38 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+class User(AbstractUser):
+    username = None
+    email = models.EmailField(unique=True)
+
+    ROLE_CHOICES = (
+        ('USER', 'Mobile User'),
+        ('ADMIN', 'Admin')
+    )
+
+    role = models.CharField(
+        max_length=20,
+        choices=ROLE_CHOICES,
+        default='USER'
+    )
+
+    phone = models.CharField(max_length=20, blank=True, null=True)
+
+    avatar = models.ImageField(
+        upload_to='users/avatars/',
+        blank=True,
+        null=True
+    )
+
+    is_verified = models.BooleanField(default=False)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+    def __str__(self):
+        return self.email
+    
+
+    @property
+    def full_name(self):
+        return f'{self.first_name} {self.last_name}'.strip()
